9a6fd027ad6aac519978eb2852a2ce00d9e5ee97
Andrea Leo <aleo@key-biz.it>
2020-04-06 13:16:45 +0200

    Added Sleuth for tracing

--- my-service/src/main/java/com/istiokeycloackintegration/myservice/MyServiceController.java
+++ my-service/src/main/java/com/istiokeycloackintegration/myservice/MyServiceController.java
@@ -1,5 +1,8 @@
 package com.istiokeycloackintegration.myservice;
 
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,7 +12,10 @@ public class MyServiceController {
 
 	@GetMapping("/hello")
 	private Mono<String> sayHello() {
+		_log.info("Hello Request");
 		return Mono.just("Hello!");
 	}
+
+	private static final Logger _log = LoggerFactory.getLogger(MyServiceController.class);
 
 }
